afbfc2a840d5f0ac3065856b454e70ae2c7bc194
robgol <rmvcorreia@gmail.com>
2023-01-05 10:02:32 -0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,2 @@
-document.querySelector("#lastUpdated").textContent =document.lastModified;
+document.querySelector("#lastUpdated").textContent = document.lastModified;
+document.querySelector("#currentYear").textContent = new Date().getFullYear();
